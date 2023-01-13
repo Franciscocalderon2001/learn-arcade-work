bca6f5d3c32f623594783d20de94f9151903648f
Francisco <Franciscocalderon1@mail.weber.edu>
2023-01-12 18:55:04 -0700

    Added more things on the code after reading chapter 4.

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -9,3 +9,15 @@ Yes, I think you've seen me before
 #print "hello world"
 print("Hello World")
 print('Hello World')
+
+print(30094 + 8339)
+
+print("When adding 20 and 5 we get (25)")
+
+print(9-23)
+print(89*2)
+print(66*3+2)
+
+print("Hello! This is a double quote mark: \" We use it for printing.")
+print("The file is stored in C:\\new folder")
+print("I \n like \n cake.")
