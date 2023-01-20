f09cd1d7e7b0a9a1d951fcd3350765a9dfbfc279
Francisco <Franciscocalderon1@mail.weber.edu>
2023-01-19 18:27:08 -0700

    Added more things on the code after reading chapter 4. Fixed a couple of things on my print statements to make it easier. Made 4 trees with different top shapes using python.

--- Lab 02 - Draw a Picture/drawing_samples.py
+++ Lab 02 - Draw a Picture/drawing_samples.py
@@ -37,7 +37,28 @@ arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
 #Tree top
 arcade.draw_circle_filled(100,350,30, arcade.csscolor.DARK_GREEN)
 
+#Draw an ellipse and rect with
+# a center of (300,300)
+# width of 350
+# height of 200
+# arcade.draw_rectangle_outline(300, 300, 350, 200, arcade.csscolor.BLACK, 3)
+# arcade.draw_ellipse_outline(300,300,350,200, arcade.csscolor.RED, 3)
 
+#Another tree, with a trunk and ellipse for top
+arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.DARK_GREEN)
+
+# Another tree, with trunk and arc for top
+# Arc is centered at (300, 340) with a width of 60 and height of 100.
+# The starting angle is 0, and ending angle is 180.
+arcade.draw_rectangle_filled(300, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_arc_filled(300, 340, 60, 100, arcade.csscolor.DARK_GREEN, 0, 180)
+
+# Another tree, with a trunk and triangle for top
+# Triangle is made of these three points:
+# (400, 400), (370, 320), (430, 320)
+arcade.draw_rectangle_filled(400, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_triangle_filled(400, 400, 370, 320, 430, 320, arcade.csscolor.DARK_GREEN)
 
 # Finish drawing
 arcade.finish_render()
