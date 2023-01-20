448ad12ba71020ad56a668d7423c207638ebe6a7
Francisco <Franciscocalderon1@mail.weber.edu>
2023-01-19 18:46:39 -0700

    Added more things on the code after reading chapter 4. Fixed a couple of things on my print statements to make it easier. Made 4 trees with different top shapes using python. Finishing up drawing the trees. Changed the way the trees were colored and positioned.

--- Lab 02 - Draw a Picture/drawing_samples.py
+++ Lab 02 - Draw a Picture/drawing_samples.py
@@ -31,37 +31,37 @@ arcade.start_render()
 # Top of 300, bottom of 0
 arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
 
-#Tree Trunk
-arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
+# Tree Trunk
+arcade.draw_rectangle_filled(100, 300, 20, 60, arcade.csscolor.ROSY_BROWN)
 
-#Tree top
-arcade.draw_circle_filled(100,350,30, arcade.csscolor.DARK_GREEN)
+# Tree top
+arcade.draw_circle_filled(100,330,30, arcade.csscolor.DARK_GREEN)
 
-#Draw an ellipse and rect with
+# Draw an ellipse and rect with
 # a center of (300,300)
 # width of 350
 # height of 200
 # arcade.draw_rectangle_outline(300, 300, 350, 200, arcade.csscolor.BLACK, 3)
 # arcade.draw_ellipse_outline(300,300,350,200, arcade.csscolor.RED, 3)
 
-#Another tree, with a trunk and ellipse for top
-arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
-arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.DARK_GREEN)
+# Another tree, with a trunk and ellipse for top
+arcade.draw_rectangle_filled(200, 100, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_ellipse_filled(200, 150, 60, 80, arcade.csscolor.DARK_GREEN)
 
 # Another tree, with trunk and arc for top
 # Arc is centered at (300, 340) with a width of 60 and height of 100.
 # The starting angle is 0, and ending angle is 180.
-arcade.draw_rectangle_filled(300, 320, 20, 60, arcade.csscolor.SIENNA)
-arcade.draw_arc_filled(300, 340, 60, 100, arcade.csscolor.DARK_GREEN, 0, 180)
+arcade.draw_rectangle_filled(300, 300, 20, 60, arcade.csscolor.BROWN)
+arcade.draw_arc_filled(300, 320, 60, 100, arcade.csscolor.DARK_GREEN, 0, 180)
 
 # Another tree, with a trunk and triangle for top
 # Triangle is made of these three points:
 # (400, 400), (370, 320), (430, 320)
-arcade.draw_rectangle_filled(400, 320, 20, 60, arcade.csscolor.SIENNA)
-arcade.draw_triangle_filled(400, 400, 370, 320, 430, 320, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(400, 100, 20, 60, arcade.csscolor.SADDLE_BROWN)
+arcade.draw_triangle_filled(400, 180, 370, 100, 430, 100, arcade.csscolor.DARK_GREEN)
 
 # Draw a tree using a polygon with a list of points
-arcade.draw_rectangle_filled(500, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_rectangle_filled(500, 300, 20, 60, arcade.csscolor.SANDY_BROWN)
 arcade.draw_polygon_filled(((500, 400),
                             (480, 360),
                             (470, 320),
