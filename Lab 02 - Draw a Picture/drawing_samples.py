8cfffd8d8c32590c5a93d1435b23f60f5d4d5c63
Francisco <Franciscocalderon1@mail.weber.edu>
2023-01-26 21:39:55 -0700

    Added more things on the code after reading chapter 4. Fixed a couple of things on my print statements to make it easier. Made 4 trees with different top shapes using python. Finishing up drawing the trees. Changed the way the trees were colored and positioned.Chapter 8 run through of creating simple functions. Creating a home. created a mountain

--- Lab 02 - Draw a Picture/drawing_samples.py
+++ Lab 02 - Draw a Picture/drawing_samples.py
@@ -1,14 +1,15 @@
 """
 This is a program that I am using to draw using python programming
-language and the Arcade library.
+language and the Arcade library. Will be using to draw an illustration
 
 As said in the learn academy chapter 5, Multi-line comments are surrounded
 by three double-quote marks.
 Single-line comments start with a hash/pound sign. #
 """
+# Created by: Francisco Calderon
 
-# these are pretty cool.
-# import math as an example
+# These are pretty cool.
+# Import math as an example
 
 import math
 
@@ -21,7 +22,7 @@ import arcade
 arcade.open_window(600, 600, "Drawing Example")
 
 # Set the background color
-arcade.set_background_color(arcade.csscolor.BLANCHED_ALMOND)
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 
 # Get ready to draw
 arcade.start_render()
@@ -31,11 +32,6 @@ arcade.start_render()
 # Top of 300, bottom of 0
 arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
 
-# Tree Trunk
-arcade.draw_rectangle_filled(100, 300, 20, 60, arcade.csscolor.ROSY_BROWN)
-
-# circle top
-arcade.draw_circle_filled(100,330,30, arcade.csscolor.DARK_GREEN)
 
 # Draw an ellipse and rect with
 # a center of (300,300)
@@ -45,22 +41,20 @@ arcade.draw_circle_filled(100,330,30, arcade.csscolor.DARK_GREEN)
 # arcade.draw_ellipse_outline(300,300,350,200, arcade.csscolor.RED, 3)
 
 # Drew a elliptic pond
-arcade.draw_ellipse_filled(170, 150, 150, 200, arcade.csscolor.DARK_GREEN)
-
-# Another tree, with trunk and arc for top
-# Arc is centered at (300, 340) with a width of 60 and height of 100.
-# The starting angle is 0, and ending angle is 180.
-arcade.draw_rectangle_filled(300, 300, 20, 60, arcade.csscolor.BROWN)
-
+arcade.draw_ellipse_filled(180, 140, 150, 200, arcade.csscolor.DARK_BLUE)
 
 # House with a triangular roof and arc door, also a two windows
 # Triangle is made of these three points:
 # (400, 400), (370, 320), (430, 320)
 arcade.draw_rectangle_filled(400, 100, 150, 150, arcade.csscolor.WHITE)
 arcade.draw_triangle_filled(400, 300, 305, 175, 500, 175, arcade.csscolor.BROWN)
-arcade.draw_arc_filled(400, 25, 80, 120, arcade.csscolor.DARK_GREEN, 0, 180)
-arcade.draw_circle_filled(440,120,25, arcade.csscolor.DARK_GREEN)
-arcade.draw_circle_filled(360,120,25, arcade.csscolor.DARK_GREEN)
+arcade.draw_arc_filled(400, 25, 80, 120, arcade.csscolor.BLANCHED_ALMOND, 0, 180)
+arcade.draw_circle_filled(440,120,25, arcade.csscolor.BLACK)
+arcade.draw_circle_filled(360,120,25, arcade.csscolor.BLACK)
+arcade.draw_circle_filled(440,120,23, arcade.csscolor.WHITE)
+arcade.draw_circle_filled(360,120,23, arcade.csscolor.WHITE)
+arcade.draw_circle_filled(410,45,5, arcade.csscolor.BLACK)
+arcade.draw_circle_filled(390,45,5, arcade.csscolor.BLACK)
 arcade.draw_rectangle_filled(400, 55, 5, 60, arcade.csscolor.BLACK)
 
 # Draw a mountain using a polygon
@@ -71,6 +65,33 @@ arcade.draw_polygon_filled(((500, 450),
                             (590, 390)
                             ),
                            arcade.csscolor.SADDLE_BROWN)
+
+# Tree with circle top all around fence of the house
+arcade.draw_rectangle_filled(50, 300, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(50,330,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(150, 300, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(150,330,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(250, 300, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(250,330,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(350, 300, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(350,330,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(450, 300, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(450,330,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(550, 300, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(550,330,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(550, 200, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(550,230,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(550, 100, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(550,130,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(550, 0, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(550,30,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(50, 200, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(50,230,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(50, 100, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(50,130,30, arcade.csscolor.DARK_GREEN)
+arcade.draw_rectangle_filled(50, 0, 20, 60, arcade.csscolor.ROSY_BROWN)
+arcade.draw_circle_filled(50,30,30, arcade.csscolor.DARK_GREEN)
+
 
 # Draw a sun
 arcade.draw_circle_filled(500, 550, 40, arcade.color.YELLOW)
