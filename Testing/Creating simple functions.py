f5ce68dca9e421b003a249e2c35a3a46db4c027f
Francisco <Franciscocalderon1@mail.weber.edu>
2023-01-26 18:28:41 -0700

    Added more things on the code after reading chapter 4. Fixed a couple of things on my print statements to make it easier. Made 4 trees with different top shapes using python. Finishing up drawing the trees. Changed the way the trees were colored and positioned.Chapter 8 run through of creating simple functions.

--- Testing/Creating simple functions.py
+++ Testing/Creating simple functions.py
@@ -1,8 +1,5 @@
 #Creating Functions
 #Francisco Calderon
-def print_hello():
-    """This is a comment that describes the function."""
-    print("Hello!")
 
 print('lost in the world')
 
@@ -10,8 +7,45 @@ def foo():
     print('foo')
 
 
-print_hello()
-print_hello()
-print_hello()
 print('this is the end')
 
+def pause():
+    #does nothing
+    pass
+#write hello
+def print_hello():
+    print("Hello!")
+#print bye
+def print_goodbye():
+    print("Bye!")
+
+x=10
+def add(y):
+    print('getting ready to add the numbers')
+    print('adding them now...')
+    ret_value = x+y
+    print('done with my work, now i will return that value')
+    print('x',x)
+    print('ret_value',ret_value)
+    return(x+y)
+    print('this is really important. I forgot to say that I am really dont now')
+
+
+#print main function
+def main():
+    """ This is my main program function """
+    print_hello()
+    pause()
+    print_goodbye()
+    z=add(8)
+    print(z)
+    print('x:',x)
+
+
+
+
+
+# Only run the main function if we are running this file. Don't run it
+# if we are importing this file.
+if __name__ == "__main__":
+    main()
