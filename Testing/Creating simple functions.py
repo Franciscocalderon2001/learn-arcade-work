f7f45481d4053e03788af1c457c826a9d3fe5699
Francisco <Franciscocalderon1@mail.weber.edu>
2023-01-26 17:52:02 -0700

    Added more things on the code after reading chapter 4. Fixed a couple of things on my print statements to make it easier. Made 4 trees with different top shapes using python. Finishing up drawing the trees. Changed the way the trees were colored and positioned.

---
+++ Testing/Creating simple functions.py
@@ -0,0 +1,17 @@
+#Creating Functions
+#Francisco Calderon
+def print_hello():
+    """This is a comment that describes the function."""
+    print("Hello!")
+
+print('lost in the world')
+
+def foo():
+    print('foo')
+
+
+print_hello()
+print_hello()
+print_hello()
+print('this is the end')
+
