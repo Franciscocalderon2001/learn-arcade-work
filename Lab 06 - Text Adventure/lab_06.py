446d7b27bf69da6b9c38f131dd6e1a3e19f4dfe2
Francisco <Franciscocalderon1@mail.weber.edu>
2023-03-18 22:16:31 -0600

    Added more things on the code after reading chapter 4. Fixed a couple of things on my print statements to make it easier. Made 4 trees with different top shapes using python. Finishing up drawing the trees. Changed the way the trees were colored and positioned.Chapter 8 run through of creating simple functions. Creating a home. created a mountain. Created a snowman that can move around.

---
+++ Lab 06 - Text Adventure/lab_06.py
@@ -0,0 +1,317 @@
+class Room:
+    def __init__(self, description: str = "", north: int = 0, east: int = 0, south: int = 0, west: int = 0):
+        self.description: str = description
+        self.north: int = north
+        self.east: int = east
+        self.south: int = south
+        self.west: int = west
+
+
+def main():
+    room_list = []
+    # main room/ intro room (0)
+    room = Room(
+        "You are at a crossroads, and you can choose to go down any of the four hallways\ngoing North, East, South, "
+        "West.",
+        None, 2, 1, 4)
+
+    room_list.append(room)  # cursed room (1)
+    room = Room(
+        "You have arrived in the cursed room.\nYou hear strange voices. You believe you have awoken some of the dead.\n"
+        "You see a hall East and one West.",
+        0, 7, None, 8)
+
+    room_list.append(room)  # dead spider room (2)
+    room = Room(
+        "You see a wall of dead spiders as you walk into the room. Somebody is watching you. "
+        "You see a door North and a hallway going East.", None, 3, None, 0)
+
+    room_list.append(room)  # strange creature room (3)
+    room = Room("A strange wolf-like creature has appeared. Run away! Head back West.",
+                None, None, None, 2)
+
+    room_list.append(room)  # ghost figure room (4)
+    room = Room(
+        "You see a dark transparent figure appear in the distance. Chills run down your spine."
+        "There is a corridor heading North and a door South of you.", 5, 0, None, None)
+
+    room_list.append(room)  # backpack room (5)
+    room = Room(
+        "You discover an empty backpack that has been dropped on the ground. Someone has been here recently.\n"
+        "There is a small passage headed North.", 6, None,
+        5, None)
+
+    room_list.append(room)  # exit number (6)
+    room = Room("You made it! You've found an exit.", None, None, 5, None)
+
+    room_list.append(room)  # exit number (7)
+    room = Room("You made it! You've found an exit.", None, None, None, 1)
+
+    room_list.append(room)  # Death room (8)
+    room = Room("Multiple blood-oozing zombies start emerging as you enter the room. You are killed.", None, 1, None, None)
+
+    room_list.append(room)
+
+    current_room = 0
+    next_room = None
+
+    done = False
+    while not done:
+        print("\n" + room_list[current_room].description)
+        user_input = input("Where would you like to go? ")
+        if user_input.upper() == "N" or user_input.upper() == "NORTH":
+            next_room = room_list[current_room].north
+        elif user_input.upper() == "E" or user_input.upper() == "EAST":
+            next_room = room_list[current_room].east
+        elif user_input.upper() == "S" or user_input.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+        elif user_input.upper() == "W" or user_input.upper() == "WEST":
+            next_room = room_list[current_room].west
+        elif user_input.upper() == "Q" or user_input.upper() == "QUIT":
+            done = True
+            print("\nYou left the catacombs\nGame Over")
+            continue
+        else:
+            print("Please enter a valid option.")
+            next_room = None
+            continue
+
+        if next_room is None:
+            print("Dead end/ Wall encountered")
+        else:
+            current_room = next_room
+
+    current_room = 1
+    next_room = None
+
+    done = False
+    while not done:
+        print("\n" + room_list[current_room].description)
+        user_input = input("Where would you like to go? ")
+        if user_input.upper() == "N" or user_input.upper() == "NORTH":
+            next_room = room_list[current_room].north
+        elif user_input.upper() == "E" or user_input.upper() == "EAST":
+            next_room = room_list[current_room].east
+        elif user_input.upper() == "S" or user_input.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+        elif user_input.upper() == "W" or user_input.upper() == "WEST":
+            next_room = room_list[current_room].west
+        elif user_input.upper() == "Q" or user_input.upper() == "QUIT":
+            done = True
+            print("\nYou left the catacombs\nGame Over")
+            continue
+        else:
+            print("Please enter a valid option.")
+            next_room = None
+            continue
+
+        if next_room is None:
+            print("Dead end/ Wall encountered")
+        else:
+            current_room = next_room
+
+    current_room = 2
+    next_room = None
+
+    done = False
+    while not done:
+        print("\n" + room_list[current_room].description)
+        user_input = input("Where would you like to go? ")
+        if user_input.upper() == "N" or user_input.upper() == "NORTH":
+            next_room = room_list[current_room].north
+        elif user_input.upper() == "E" or user_input.upper() == "EAST":
+            next_room = room_list[current_room].east
+        elif user_input.upper() == "S" or user_input.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+        elif user_input.upper() == "W" or user_input.upper() == "WEST":
+            next_room = room_list[current_room].west
+        elif user_input.upper() == "Q" or user_input.upper() == "QUIT":
+            done = True
+            print("\nYou left the catacombs\nGame Over")
+            continue
+        else:
+            print("Please enter a valid option.")
+            next_room = None
+            continue
+
+        if next_room is None:
+            print("Dead end/ Wall encountered")
+        else:
+            current_room = next_room
+
+    current_room = 3
+    next_room = None
+
+    done = False
+    while not done:
+        print("\n" + room_list[current_room].description)
+        user_input = input("Where would you like to go? ")
+        if user_input.upper() == "N" or user_input.upper() == "NORTH":
+            next_room = room_list[current_room].north
+        elif user_input.upper() == "E" or user_input.upper() == "EAST":
+            next_room = room_list[current_room].east
+        elif user_input.upper() == "S" or user_input.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+        elif user_input.upper() == "W" or user_input.upper() == "WEST":
+            next_room = room_list[current_room].west
+        elif user_input.upper() == "Q" or user_input.upper() == "QUIT":
+            done = True
+            print("\nYou left the catacombs\nGame Over")
+            continue
+        else:
+            print("Please enter a valid option.")
+            next_room = None
+            continue
+
+        if next_room is None:
+            print("Dead end/ Wall encountered")
+        else:
+            current_room = next_room
+
+    current_room = 4
+    next_room = None
+
+    done = False
+    while not done:
+        print("\n" + room_list[current_room].description)
+        user_input = input("Where would you like to go? ")
+        if user_input.upper() == "N" or user_input.upper() == "NORTH":
+            next_room = room_list[current_room].north
+        elif user_input.upper() == "E" or user_input.upper() == "EAST":
+            next_room = room_list[current_room].east
+        elif user_input.upper() == "S" or user_input.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+        elif user_input.upper() == "W" or user_input.upper() == "WEST":
+            next_room = room_list[current_room].west
+        elif user_input.upper() == "Q" or user_input.upper() == "QUIT":
+            done = True
+            print("\nYou left the catacombs\nGame Over")
+            continue
+        else:
+            print("Please enter a valid option.")
+            next_room = None
+            continue
+
+        if next_room is None:
+            print("Dead end/ Wall encountered")
+        else:
+            current_room = next_room
+
+    current_room = 5
+    next_room = None
+
+    done = False
+    while not done:
+        print("\n" + room_list[current_room].description)
+        user_input = input("Where would you like to go? ")
+        if user_input.upper() == "N" or user_input.upper() == "NORTH":
+            next_room = room_list[current_room].north
+        elif user_input.upper() == "E" or user_input.upper() == "EAST":
+            next_room = room_list[current_room].east
+        elif user_input.upper() == "S" or user_input.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+        elif user_input.upper() == "W" or user_input.upper() == "WEST":
+            next_room = room_list[current_room].west
+        elif user_input.upper() == "Q" or user_input.upper() == "QUIT":
+            done = True
+            print("\nYou left the catacombs\nGame Over")
+            continue
+        else:
+            print("Please enter a valid option.")
+            next_room = None
+            continue
+
+        if next_room is None:
+            print("Dead end/ Wall encountered")
+        else:
+            current_room = next_room
+
+    current_room = 6
+    next_room = None
+
+    done = False
+    while not done:
+        print("\n" + room_list[current_room].description)
+        user_input = input("Where would you like to go? ")
+        if user_input.upper() == "N" or user_input.upper() == "NORTH":
+            next_room = room_list[current_room].north
+        elif user_input.upper() == "E" or user_input.upper() == "EAST":
+            next_room = room_list[current_room].east
+        elif user_input.upper() == "S" or user_input.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+        elif user_input.upper() == "W" or user_input.upper() == "WEST":
+            next_room = room_list[current_room].west
+        elif user_input.upper() == "Q" or user_input.upper() == "QUIT":
+            done = True
+            print("\nYou left the catacombs\nGame Over")
+            continue
+        else:
+            print("Please enter a valid option.")
+            next_room = None
+            continue
+
+        if next_room is None:
+            print("Dead end/ Wall encountered")
+        else:
+            current_room = next_room
+
+    current_room = 7
+    next_room = None
+
+    done = False
+    while not done:
+        print("\n" + room_list[current_room].description)
+        user_input = input("Where would you like to go? ")
+        if user_input.upper() == "N" or user_input.upper() == "NORTH":
+            next_room = room_list[current_room].north
+        elif user_input.upper() == "E" or user_input.upper() == "EAST":
+            next_room = room_list[current_room].east
+        elif user_input.upper() == "S" or user_input.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+        elif user_input.upper() == "W" or user_input.upper() == "WEST":
+            next_room = room_list[current_room].west
+        elif user_input.upper() == "Q" or user_input.upper() == "QUIT":
+            done = True
+            print("\nYou left the catacombs\nGame Over")
+            continue
+        else:
+            print("Please enter a valid option.")
+            next_room = None
+            continue
+
+        if next_room is None:
+            print("Dead end/ Wall encountered")
+        else:
+            current_room = next_room
+
+    current_room = 8
+    next_room = None
+
+    done = False
+    while not done:
+        print("\n" + room_list[current_room].description)
+        user_input = input("Where would you like to go? ")
+        if user_input.upper() == "N" or user_input.upper() == "NORTH":
+            next_room = room_list[current_room].north
+        elif user_input.upper() == "E" or user_input.upper() == "EAST":
+            next_room = room_list[current_room].east
+        elif user_input.upper() == "S" or user_input.upper() == "SOUTH":
+            next_room = room_list[current_room].south
+        elif user_input.upper() == "W" or user_input.upper() == "WEST":
+            next_room = room_list[current_room].west
+        elif user_input.upper() == "Q" or user_input.upper() == "QUIT":
+            done = True
+            print("\nYou left the catacombs\nGame Over")
+            continue
+        else:
+            print("Please enter a valid option.")
+            next_room = None
+            continue
+
+        if next_room is None:
+            print("Dead end/ Wall encountered")
+        else:
+            current_room = next_room
+
+
+main()
