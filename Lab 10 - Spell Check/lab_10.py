a45eaf73cf66b3a7e34ff797d6a009157eab90d3
Francisco <Franciscocalderon1@mail.weber.edu>
2023-04-05 21:13:01 -0600

    Added more things on the code after reading chapter 4. Fixed a couple of things on my print statements to make it easier. Made 4 trees with different top shapes using python. Finishing up drawing the trees. Changed the way the trees were colored and positioned.Chapter 8 run through of creating simple functions. Creating a home. created a mountain. Created a snowman that can move around.

---
+++ Lab 10 - Spell Check/lab_10.py
@@ -0,0 +1,48 @@
+import re
+
+print("--- Linear Search ---")
+
+
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+
+
+dictionary_list = []
+with open("dictionary.txt", "r") as file:
+    for line in file:
+        dictionary_list.append(line.strip().upper())
+
+with open("AliceInWonderLand200.txt", "r") as file:
+    line_number = 0
+    for line in file:
+        line_number += 1
+        word_list = split_line(line)
+        for word in word_list:
+            if word.upper() not in dictionary_list:
+                print(f"Line {line_number} possible missed word: {word}")
+
+print("--- Binary Search ---")
+
+
+def binary_search(word, dictionary_list):
+    lower_bound = 0
+    upper_bound = len(dictionary_list) - 1
+    while lower_bound <= upper_bound:
+        middle_pos = (lower_bound + upper_bound) // 2
+        if dictionary_list[middle_pos] < word:
+            lower_bound = middle_pos + 1
+        elif dictionary_list[middle_pos] > word:
+            upper_bound = middle_pos - 1
+        else:
+            return True
+    return False
+
+
+with open("AliceInWonderLand200.txt", "r") as file:
+    line_number = 0
+    for line in file:
+        line_number += 1
+        word_list = split_line(line)
+        for word in word_list:
+            if not binary_search(word.upper(), dictionary_list):
+                print(f"Line {line_number}: {word}")
